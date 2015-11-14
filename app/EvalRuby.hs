3f38c2665cc741467f87613d5a75e492f06b6ef9
David Pedersen <david.pdrsn@gmail.com>
2015-11-14 11:10:05 +0100

    Use new json response monad in more places

--- app/EvalRuby.hs
+++ app/EvalRuby.hs
@@ -5,38 +5,27 @@ module EvalRuby
 
 import Import
 import HttpHelpers
-import qualified Data.ByteString.Char8 as BS
-import Data.CaseInsensitive
-import Control.Monad.Trans.Except
-import Data.Aeson
 import Data.Aeson.Lens
 import Control.Lens
 
 evalRuby :: String -> IO (Either GenericException String)
 evalRuby ruby = do
-    let params = [ ("cmd", Just $ BS.pack ruby)
-                 ]
-    initReq <- parseUrl "http://tryruby.org/levels/1/challenges/0"
-    let req = setQueryString params $ initReq { method = "PUT"
-                                              , requestHeaders = [("Content-Length" :: CI ByteString, "0")]
-                                              }
-    result <- runExceptT $ do
-      response <- ExceptT $ safeHttpLbs req
-      let body = responseBody response
-          output = case decode body of
-                     Nothing -> Left "Unknown error"
-                     Just v -> parseCommand v
-
-          parseCommand :: Value -> Either String String
-          parseCommand v = case v ^? key "success" . _Bool of
-                             Just True -> case unpack <$> v ^? key "output" . _String of
-                                            Nothing -> Left "Unknown error"
-                                            Just res -> Right res
-                             _ -> case unpack <$> v ^? key "result" . _String of
-                                    Nothing -> Left "Unknown error"
-                                    Just res -> Left res
-      return output
+    let req = mkReq { reqDefUrl = "http://tryruby.org/levels/1/challenges/0"
+                    , reqDefQueryParams = Just [("cmd", ruby)]
+                    , reqDefMethod = Just "PUT"
+                    , reqDefHeaders = Just [("Content-Length", "0")]
+                    }
+    result <- runJsonRequest $ parseCommand <$> fetchJson req
     case result of
       Left e -> return $ Left e
       Right (Left e) -> return $ Left $ GenericException e
       Right (Right x) -> return $ Right x
+
+parseCommand :: Value -> Either String String
+parseCommand v = case v ^? key "success" . _Bool of
+                   Just True -> case unpack <$> v ^? key "output" . _String of
+                                  Nothing -> Left "Unknown error"
+                                  Just res -> Right res
+                   _ -> case unpack <$> v ^? key "result" . _String of
+                          Nothing -> Left "Unknown error"
+                          Just res -> Left res
--- app/Giphy.hs
+++ app/Giphy.hs
@@ -5,40 +5,27 @@ module Giphy
 
 import Import hiding (httpLbs, newManager)
 import System.Environment
-import Network.URI.Encode (encodeTextToBS)
-import qualified Data.ByteString.Char8 as BS
-import qualified Data.ByteString.Lazy as LBS
-import Data.Aeson hiding (json)
 import Data.Aeson.Lens
 import Control.Lens hiding (from, to)
 import HttpHelpers
-import Control.Monad.Trans.Except
 
 gifMe :: String -> IO (Either GenericException String)
-gifMe query = do
-    url <- runExceptT $ do
-      response <- ExceptT $ search query
-      let
-        json :: Maybe Text
-        json = decode response >>= parse
+gifMe query =
+    do
+    apiKey <- getApiKey
+    let req = mkReq { reqDefUrl = "http://api.giphy.com/v1/gifs/search"
+                    , reqDefQueryParams = Just [ ("api_key", cs apiKey)
+                                               , ("q", query)
+                                               ]
+                    }
+    url <- runJsonRequest $ parseSearchResult <$> fetchJson req
+    case url of
+      Right (Just x) -> return $ Right $ cs x
+      Right Nothing -> return $ Left $ GenericException "Error parsing json"
+      Left e -> return $ Left e
 
-        parse :: Value -> Maybe Text
-        parse v = v ^? key "data" . nth 0 . key "images" . key "original" . key "url" . _String
-      case json of
-        Nothing -> ExceptT $ return $ Left $ GenericException "Error parsing json"
-        Just x -> ExceptT $ return $ Right x
-    return $ unpack <$> url
-
-search :: String -> IO (Either GenericException LBS.ByteString)
-search query = do
-    apiKey <- encodeTextToBS <$> getApiKey
-    let url = "http://api.giphy.com/v1/gifs/search"
-        params = [ ("api_key", Just apiKey)
-                 , ("q", Just $ BS.pack query)
-                 ]
-    initReq <- parseUrl url
-    let req = setQueryString params $ initReq { method = "GET" }
-    fmap responseBody <$> safeHttpLbs req
+parseSearchResult :: Value -> Maybe Text
+parseSearchResult v = v ^? key "data" . nth 0 . key "images" . key "original" . key "url" . _String
 
 getApiKey :: IO Text
 getApiKey = pack <$> fromMaybe (error "Missing env var TONSS_GIPHY_API_KEY") <$> lookupEnv "TONSS_GIPHY_API_KEY"
