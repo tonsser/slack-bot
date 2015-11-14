f2052762fbd591eed92b75a71959e2d94524fa17
David Pedersen <david.pdrsn@gmail.com>
2015-11-14 16:48:15 +0100

    Remove file used for testing

--- github.sh
+++
@@ -1,4 +0,0 @@
-curl -X POST \
-     -d '{"title":"Just testing github api"}' \
-     -v \
-     "https://api.github.com/repos/tonsser/tonss/issues?access_token=a9078b8de6a052d776f74e5da18294c4e3ad215d"
